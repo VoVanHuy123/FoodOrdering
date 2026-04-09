d56051fdcb648a0da89ca96abf7d204c0726b929
Duy Anh <simpsonbard1112@gmail.com>
2026-04-09 20:37:42 +0700

    chỉnh sửa lại quyền

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,9 +1,11 @@
 ﻿using FoodOrdering.DTOs;
 using FoodOrdering.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodOrdering.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class CategoriesController : Controller
     {
         private readonly ICategoriesService _service;
--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -1,9 +1,11 @@
 ﻿using FoodOrdering.Services.Interfaces;
 using FoodOrdering.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodOrdering.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ReportsController : Controller
     {
         private readonly IReportService _reportService;
